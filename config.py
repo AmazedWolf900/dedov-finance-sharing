8c96b3e09a1791eb2396ef0350933c6a33ffea09
Tomas Weber <me@tomas-weber.eu>
2023-12-23 22:56:48 +0100

    db host environment variable

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
     ENV_DB_NAME = os.getenv('MARIADB_DATABASE', 'finance')
     ENV_DB_USER = os.getenv('MARIADB_USER', 'finance')
     ENV_DB_PASS = os.getenv('MARIADB_PASSWORD', 'finance')
+    ENV_DB_HOST = os.getenv('MARIADB_PASSWORD', 'db')
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + ENV_DB_USER + ':' + ENV_DB_PASS + '@db:3306/' + ENV_DB_NAME
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://' + ENV_DB_USER + ':' + ENV_DB_PASS + '@' + ENV_DB_HOST + ':3306/' + ENV_DB_NAME
     SQLALCHEMY_TRACK_MODIFICATIONS = False
