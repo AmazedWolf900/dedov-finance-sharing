233ab3f8c60b594ba27174297d70ec2a24600add
Tomas Weber <me@tomas-weber.eu>
2023-12-24 22:31:41 +0100

    attachment statistics

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -1,12 +1,19 @@
 from app.blueprints.main import bp
 
 from flask import render_template
+from sqlalchemy import func
 
-from app.models.models import Project
+from app.models.models import Project, Attachment
 
 @bp.route("/")
 def index():
     project_count = Project.query.count()
+
+    attachment_count = Attachment.query.count()
+    attachment_total_file_size = Attachment.query.with_entities(func.sum(Attachment.file_size).label("fs_total")).first()[0]
+
     return render_template("main/index.html",
-                project_count=project_count
+                project_count = project_count,
+                attachment_count = attachment_count,
+                attachment_total_file_size = attachment_total_file_size
     )
